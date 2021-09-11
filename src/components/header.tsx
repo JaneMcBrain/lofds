4d9e3418721ae6c14b7b560ea7708a8e388d5391
Janine Semmler <syries@gmx.de>
2021-09-11 23:09:23 +0200

    add styled components to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  text-decoration: none;
+  ${props => (props.left ? 'margin-right' : 'margin-left')}: 10px;
+`;
+
+const HeaderTag = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  max-width: 200px;
+`;
 
 function Header(){
   const navLeft = [
@@ -11,12 +27,13 @@ function Header(){
     {name: 'System B', anchor: ''},
     {name: 'SaxophonRetrait', anchor: ''},
   ]
+
   return (
-    <header className="header">
-      {navLeft.map( item => <a href={'#' + item.anchor} className="header__item header__item--left">{item.name}</a>)}
-      <img className="header__logo" src="https://static.wixstatic.com/media/d1bfd1_395f2a29389245deb2ad542d2fde779f~mv2.jpg/v1/fill/w_895,h_295,al_c,lg_1,q_80/Lofds%20Logog.webp"></img>
-      {navRight.map( item => <a href={'#' + item.anchor} className="header__item header__item--right">{item.name}</a>)}
-    </header>
+    <HeaderTag>
+      {navLeft.map( item => <Link href={'#' + item.anchor} left>{item.name}</Link>)}
+      <Image src="https://static.wixstatic.com/media/d1bfd1_395f2a29389245deb2ad542d2fde779f~mv2.jpg/v1/fill/w_895,h_295,al_c,lg_1,q_80/Lofds%20Logog.webp"></Image>
+      {navRight.map( item => <Link href={'#' + item.anchor} right>{item.name}</Link>)}
+    </HeaderTag>
   );
 }
 
