f4dc941989be8aea49d9c70ea6bbf06a464e322d
Janine Semmler <syries@gmx.de>
2022-05-15 23:52:34 +0200

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,11 @@ import FooterContent from './content/FooterContent';
 export default function Footer(){
   const Footer = styled.footer`
     z-index: 0;
-    padding-right: 9vw;
-    padding-left: 9vw;
   `;
 
   return (
     <Footer className="fixed top0 right0 bottom0 left0 gradient-bg-h">
-      <FooterContent classes="absolute bottom0 flex" />
+      <FooterContent classes="absolute bottom0 flex footer-padding" />
     </Footer>
   );
 }
